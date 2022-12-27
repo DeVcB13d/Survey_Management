6d62b2b03c3009ac65e9097b2b77885a2d23d873
DeVcB13d <devcb@pg.cusat.ac.in>
2022-12-27 07:33:22 +0100

    add 1

---
+++ src/main/java/com/mycompany/survey_management/Survey_Management.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.survey_management;
+
+/**
+ *
+ * @author USER
+ */
+public class Survey_Management {
+
+    public static void main(String[] args) {
+        login_page Start = new login_page();
+        Start.setVisible(true);
+        //this.setVisible(false);
+        
+    }
+}
